4157aeb3e0cc0c8fbfc01dd756f03a10957c1415
Jose Valenzuela <jivalenzuela2010@hotmail.com>
2026-06-09 14:12:34 -0400

    model of user created

---
+++ backend/src/main/java/com/example/usuarios/model/Usuario.java
@@ -0,0 +1,24 @@
+package com.example.usuarios.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false,unique = true)
+    private String email;
+}
